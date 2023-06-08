9850f817d2c1bf764bf46c7c3effac6c67ff2789
codivinity <codivinity2023@gmail.com>
2023-06-08 11:12:28 +0330

    complete movie-searcher

---
+++ script.js
@@ -0,0 +1,347 @@
+var movieData = [
+    {
+        imageSrc: "./images/movie-image-1.jpg",
+        imageAlt: "movie-image-1",
+        category: "Action",
+        title: "Mission: Impossible - Fallout",
+        rating: "7.8",
+        year: "2018",
+        duration: "2h 27min",
+        genre: "Action, Adventure, Thriller",
+        description: "Ethan Hunt and his IMF team, along with some familiar allies, race against time after a mission gone wrong.",
+        ratingText: "7.8/10",
+        mood: "Intense"
+    },
+    {
+        imageSrc: "./images/movie-image-2.jpg",
+        imageAlt: "movie-image-2",
+        category: "Adventure",
+        title: "Indiana Jones and the Raiders of the Lost Ark",
+        rating: "8.4",
+        year: "1981",
+        duration: "1h 55min",
+        genre: "Action, Adventure",
+        description: "Archaeologist and adventurer Indiana Jones is hired by the U.S. government to find the Ark of the Covenant before the Nazis.",
+        ratingText: "8.4/10",
+        mood: "Exciting"
+    },
+    {
+        imageSrc: "./images/movie-image-3.jpg",
+        imageAlt: "movie-image-3",
+        category: "Action",
+        title: "The Dark Knight",
+        rating: "9.0",
+        year: "2008",
+        duration: "2h 32min",
+        genre: "Action, Crime, Drama",
+        description: "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
+        ratingText: "9.0/10",
+        mood: "Gritty"
+    },
+    {
+        imageSrc: "./images/movie-image-4.jpg",
+        imageAlt: "movie-image-4",
+        category: "Action",
+        title: "John Wick",
+        rating: "7.4",
+        year: "2014",
+        duration: "1h 41min",
+        genre: "Action, Crime, Thriller",
+        description: "An ex-hitman comes out of retirement to track down the gangsters that took everything from him.",
+        ratingText: "7.4/10",
+        mood: "Intense"
+    },
+    {
+        imageSrc: "./images/movie-image-5.jpg",
+        imageAlt: "movie-image-5",
+        category: "Adventure",
+        title: "The Lord of the Rings: The Fellowship of the Ring",
+        rating: "8.8",
+        year: "2001",
+        duration: "2h 58min",
+        genre: "Adventure, Drama, Fantasy",
+        description: "A meek Hobbit from the Shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle-earth from the Dark Lord Sauron.",
+        ratingText: "8.8/10",
+        mood: "Epic"
+    },
+    {
+        imageSrc: "./images/movie-image-6.jpg",
+        imageAlt: "movie-image-6",
+        category: "Action",
+        title: "The Avengers",
+        rating: "8.0",
+        year: "2012",
+        duration: "2h 23min",
+        genre: "Action, Adventure, Sci-Fi",
+        description: "Earth's mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity.",
+        ratingText: "8.0/10",
+        mood: "Thrilling"
+    },
+    {
+        imageSrc: "./images/movie-image-7.jpg",
+        imageAlt: "movie-image-7",
+        category: "Sci-fi",
+        title: "Blade Runner",
+        rating: "8.1",
+        year: "1982",
+        duration: "1h 57min",
+        genre: "Sci-Fi, Thriller",
+        description: "A blade runner must pursue and terminate four replicants who stole a ship in space and have returned to Earth to find their creator.",
+        ratingText: "8.1/10",
+        mood: "Atmospheric"
+    },
+    {
+        imageSrc: "./images/movie-image-8.jpg",
+        imageAlt: "movie-image-8",
+        category: "Comedy",
+        title: "The Hangover",
+        rating: "7.7",
+        year: "2009",
+        duration: "1h 40min",
+        genre: "Comedy",
+        description: "Three buddies wake up from a bachelor party in Las Vegas, with no memory of the previous night and the bachelor missing. They make their way around the city in order to find their friend before his wedding.",
+        ratingText: "7.7/10",
+        mood: "Hilarious"
+    },
+    {
+        imageSrc: "./images/movie-image-9.jpg",
+        imageAlt: "movie-image-9",
+        category: "Sci-fi",
+        title: "The Matrix",
+        rating: "8.7",
+        year: "1999",
+        duration: "2h 16min",
+        genre: "Action, Sci-Fi",
+        description: "A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.",
+        ratingText: "8.7/10",
+        mood: "Mind-bending"
+    },
+    {
+        imageSrc: "./images/movie-image-10.jpg",
+        imageAlt: "movie-image-10",
+        category: "Comedy",
+        title: "Superbad",
+        rating: "7.6",
+        year: "2007",
+        duration: "1h 53min",
+        genre: "Comedy",
+        description: "Two co-dependent high school seniors are forced to deal with separation anxiety after their plan to stage a booze-soaked party goes awry.",
+        ratingText: "7.6/10",
+        mood: "Hilarious"
+    },
+    {
+        imageSrc: "./images/movie-image-11.jpg",
+        imageAlt: "movie-image-11",
+        category: "Sci-fi",
+        title: "Inception",
+        rating: "8.8",
+        year: "2010",
+        duration: "2h 28min",
+        genre: "Action, Adventure, Sci-Fi",
+        description: "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.",
+        ratingText: "8.8/10",
+        mood: "Mind-bending"
+    },
+    {
+        imageSrc: "./images/movie-image-12.jpg",
+        imageAlt: "movie-image-12",
+        category: "Comedy",
+        title: "Anchorman: The Legend of Ron Burgundy",
+        rating: "7.2",
+        year: "2004",
+        duration: "1h 34min",
+        genre: "Comedy",
+        description: "Ron Burgundy is San Diego's top-rated newsman in the male-dominated broadcasting of the 1970s, but that's all about to change for Ron and his cronies when an ambitious woman is hired as a new anchor.",
+        ratingText: "7.2/10",
+        mood: "Hilarious"
+    },
+    {
+        imageSrc: "./images/movie-image-13.jpg",
+        imageAlt: "movie-image-13",
+        category: "Sci-fi",
+        title: "The Martian",
+        rating: "8.0",
+        year: "2015",
+        duration: "2h 24min",
+        genre: "Adventure, Drama, Sci-Fi",
+        description: "An astronaut becomes stranded on Mars after his team assumes him dead and must rely on his ingenuity to find a way to signal to Earth that he is alive.",
+        ratingText: "8.0/10",
+        mood: "Thrilling"
+    },
+    {
+        imageSrc: "./images/movie-image-1.jpg",
+        imageAlt: "movie-image-14",
+        category: "Comedy",
+        title: "Deadpool",
+        rating: "8.0",
+        year: "2016",
+        duration: "1h 49min",
+        genre: "Action, Adventure, Comedy",
+        description: "A wisecracking mercenary gets experimented on and becomes immortal but ugly, and sets out to track down the man who ruined his looks.",
+        ratingText: "8.0/10",
+        mood: "Hilarious"
+    }
+];
+
+
+function createMovieCard(imageSrc, imageAlt, category, title, rating, year, duration, genre, description, ratingText, mood) {
+    // Create the article element
+    var article = document.createElement("article");
+    article.className = "movie-card";
+
+    // Create the image container
+    var imageContainer = document.createElement("div");
+    imageContainer.className = "image-container";
+    article.appendChild(imageContainer);
+
+    // Create the link and image elements
+    var link = document.createElement("a");
+    link.href = "";
+    imageContainer.appendChild(link);
+
+    var image = document.createElement("img");
+    image.className = "movie-image";
+    image.src = imageSrc;
+    image.alt = imageAlt;
+    link.appendChild(image);
+
+    // Create the movie title container
+    var titleContainer = document.createElement("div");
+    titleContainer.className = "movie-item__title-in";
+    imageContainer.appendChild(titleContainer);
+
+    // Create the category span
+    var categoryDiv = document.createElement("div");
+    categoryDiv.className = "cate";
+    var categorySpan = document.createElement("span");
+    var categoryLink = document.createElement("a");
+    categoryLink.href = "#";
+    categoryLink.textContent = category;
+    categorySpan.appendChild(categoryLink);
+    categoryDiv.appendChild(categorySpan)
+    titleContainer.appendChild(categoryDiv);
+
+    // Create the movie title heading
+    var titleHeading = document.createElement("h6");
+    titleHeading.className = "movie-item__title-in__heading";
+    var titleHeadingLink = document.createElement("a");
+    titleHeadingLink.href = "#";
+    titleHeadingLink.textContent = title;
+    titleHeading.appendChild(titleHeadingLink);
+    titleContainer.appendChild(titleHeading);
+
+    // Create the movie rating
+    var ratingElement = document.createElement("p");
+    var ratingIcon = document.createElement("i");
+    ratingIcon.className = "fa-fw fa-md fa-star fa";
+    var ratingValue = document.createElement("span");
+    ratingValue.textContent = rating;
+    ratingElement.appendChild(ratingIcon);
+    ratingElement.appendChild(ratingValue);
+    ratingElement.appendChild(document.createTextNode("/10"));
+    titleContainer.appendChild(ratingElement);
+
+    // Create the detail container
+    var detailContainer = document.createElement("div");
+    detailContainer.className = "detail-container";
+    article.appendChild(detailContainer);
+
+    // Create the movie heading
+    var movieHeading = document.createElement("p");
+    movieHeading.className = "movie-heading";
+    movieHeading.textContent = title + " (" + year + ")";
+    detailContainer.appendChild(movieHeading);
+
+    // Create the movie info
+    var info = document.createElement("div");
+    info.className = "info";
+    var infoText = document.createElement("span");
+    infoText.textContent = duration + " | " + genre;
+    info.appendChild(infoText);
+    detailContainer.appendChild(info);
+
+    // Create the movie description
+    var descriptionElement = document.createElement("p");
+    descriptionElement.className = "description";
+    descriptionElement.textContent = description;
+    detailContainer.appendChild(descriptionElement);
+
+    // Create the movie rate
+    var rate = document.createElement("p");
+    rate.className = "rate";
+    rate.textContent = "Rating: " + ratingText + " | Mood: " + mood;
+    detailContainer.appendChild(rate);
+
+    // Create the buttons container
+    var buttons = document.createElement("div");
+    buttons.className = "buttons";
+    detailContainer.appendChild(buttons);
+
+    // Create the trailer button
+    var trailerButton = document.createElement("button");
+    trailerButton.type = "button";
+    trailerButton.className = "button";
+    trailerButton.textContent = "TRAILER";
+    buttons.appendChild(trailerButton);
+
+    // Create the IMBD button
+    var imdbButton = document.createElement("button");
+    imdbButton.type = "button";
+    imdbButton.className = "button";
+    imdbButton.textContent = "IMBD";
+    buttons.appendChild(imdbButton);
+
+    // Return the created HTML structure
+    return article;
+}
+
+const removeAllCards = () => {
+    movieGrid.replaceChildren();
+};
+
+// Usage example:
+var movieCard = createMovieCard(
+    "./images/movie-image-1.jpg",
+    "movie-image-1",
+    "Sci-fi",
+    "Interstaller",
+    "7.4",
+    "2015",
+    "2h 2min",
+    "Crime, Drama",
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni dolore similique neque omnis. Nobis non exercitationem ducimus distinctio perspiciatis odit dicta. Reiciendis praesentium ex, ducimus beatae non repellat autem! Consequuntur.",
+    "7.4/10",
+    "Dark"
+);
+
+
+const movieGrid = document.querySelector(".movie-grid")
+
+function submitForm(event) {
+    event.preventDefault();
+    removeAllCards();
+
+    let genre = document.querySelector("form").elements["genres"].value;
+
+    let filteredMovies = movieData.filter(function (movie) {
+        return movie.genre.includes(genre);
+    });
+
+    filteredMovies.forEach(function (movie) {
+        let movieCard = createMovieCard(
+            movie.imageSrc,
+            movie.imageAlt,
+            movie.category,
+            movie.title,
+            movie.rating,
+            movie.year,
+            movie.duration,
+            movie.genre,
+            movie.description,
+            movie.ratingText,
+            movie.mood
+        );
+        movieGrid.appendChild(movieCard)
+    })
+
+}
